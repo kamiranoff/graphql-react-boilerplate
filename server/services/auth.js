b70c5b682b4247f7f6da7c0d14e8590f46a39e4d
Kevin Amiranoff <kamiranoff@gmail.com>
2017-03-15 00:40:51 +0100

    adds signup, login and logout mutations

--- server/services/auth.js
+++ server/services/auth.js
@@ -81,4 +81,11 @@ function login({ email, password, req }) {
   });
 }
 
-module.exports = { signup, login };
+function logout(req) {
+  // saving the user just so we can retun it to graphql
+  const { user } = req;
+  req.logout();
+  return user;
+}
+
+module.exports = { signup, login, logout };
